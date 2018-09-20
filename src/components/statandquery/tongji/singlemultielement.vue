8a687ef0d4bbe43c41ffc0635ce227494f125cb8
Fred梁发鹏 <fredtwins@163.com>
2018-09-20 09:49:48 +0800

    单站元素对比图完成

--- src/components/statandquery/tongji/singlemultielement.vue
+++ src/components/statandquery/tongji/singlemultielement.vue
@@ -21,6 +21,7 @@
                 <Date-picker type="datetime" v-model="formInline.datetime2" @on-change="datetimeChange2"></Date-picker>
               </Form-item>
               <Button type="primary" @click="submitSearch(1)">搜索</Button>
+              <Button type="success" @click="gettownecharts">单站搜索</Button>
             </Form>
             </template>
           </Panel>
@@ -31,6 +32,14 @@
         <!-- <div class="pullright page">
           <Page :total="total" :current="current" @on-change="changePage"></Page>
         </div> -->
+        <template>
+          <Modal v-model="looklists" class="modalx">
+            <div slot="header" ref="inter">{{updateTitle}}</div>
+            <Form :model="formItem">
+              <div id="main" class="main" style="width: 72rem;height: 530px"></div>
+            </Form>
+          </Modal>
+        </template>
       </div>
     </div>
   </i-col>
@@ -38,12 +47,16 @@
 
 <script>
 import { GettownList } from 'api/stationlist'
-import { Getmultielement } from 'api/statistics'
+import { Getmultielement, GetHistorcal } from 'api/statistics'
 import { ERR_OK } from 'api/config'
 import { multielementThead } from 'common/js/table'
+import echarts from 'echarts'
 export default {
   data () {
     return {
+      looklists: false,
+      formItem: {},
+      updateTitle: '',
       loading: false,
       dataSourceList: [],
       theadArr: multielementThead(this),
@@ -91,6 +104,106 @@ export default {
     },
     datetimeChange2 (datetime) {
       this.formInline.datetime2 = datetime
+    },
+    // echarts图查询
+    gettownecharts () {
+      this.looklists = true
+      this.updateTitle = '元素图'
+      let search = {}
+      if (this.formInline.id) {
+        search.stationid = this.formInline.id
+      }
+      if (this.formInline.datetime1 || this.formInline.datetime2) {
+        search.datetime = this.formInline.datetime1 + '|#|' + this.formInline.datetime2
+      }
+      GetHistorcal(search).then(res => {
+        if (res.code === ERR_OK) {
+          this.GetEchartsList(res.result.result)
+        }
+      })
+    },
+    // echarts图
+    GetEchartsList (obj) {
+      let option = {}
+      var qiwen = []
+      var qiya = []
+      var shidu = []
+      var dianya = []
+      var rain = []
+      var timesecharts = []
+      let myChart = echarts.init(document.getElementById('main'))
+      // 循环单站的名字
+      for (var i = 0; i < obj.length; i++) {
+        qiwen.push(obj[i].t)
+        qiya.push(obj[i].p)
+        shidu.push(obj[i].u)
+        dianya.push(obj[i].wpv)
+        rain.push(obj[i].r24h)
+        timesecharts.push(obj[i].datetime)
+      }
+      option = {
+        title: {
+          text: '折线图堆叠'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['气温', '气压', '相对湿度', '电压', '雨量']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: timesecharts
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '气温',
+            type: 'line',
+            stack: '总量',
+            data: qiwen
+          },
+          {
+            name: '气压',
+            type: 'line',
+            stack: '总量',
+            data: qiya
+          },
+          {
+            name: '相对湿度',
+            type: 'line',
+            stack: '总量',
+            data: shidu
+          },
+          {
+            name: '电压',
+            type: 'line',
+            stack: '总量',
+            data: dianya
+          },
+          {
+            name: '雨量',
+            type: 'line',
+            stack: '总量',
+            data: rain
+          }
+        ]
+      }
+      myChart.setOption(option)
     }
   },
   mounted() {
